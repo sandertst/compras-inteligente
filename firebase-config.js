0a9e9c8e8b3782ec970abcbb6afe8b6445bbff93
sandertst <115425189+sandertst@users.noreply.github.com>
2026-06-03 21:56:28 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
-window.firebaseSettings = {
-  enabled: false,
-  projectId: "",
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  appId: "",
-  shoppingListId: "compras-casa"
+const firebaseConfig = {
+  apiKey: "AIzaSyCZuJkIcM-netmX0cgH0EbRAmD41JEwgIk",
+  authDomain: "compras-inteligente-9598d.firebaseapp.com",
+  databaseURL: "https://compras-inteligente-9598d-default-rtdb.firebaseio.com",
+  projectId: "compras-inteligente-9598d",
+  storageBucket: "compras-inteligente-9598d.firebasestorage.app",
+  messagingSenderId: "176407786454",
+  appId: "1:176407786454:web:c86aacdc004472d4646a77"
 };
